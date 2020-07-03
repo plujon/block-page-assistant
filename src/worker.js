12ff29796db39cce8f66f524ea7d800754a27b7f
Jon Wilkes <jon@pluckeye.net>
2020-07-03 15:31:45 -0700

    Simplify src/worker.js for Firefox, Safari, etc.
    The former implementation tried to do much more (I didn't very hard to
    understand it), but it apparently didn't work in Firefox.  Upcoming
    support for webextensions in Sarari caused Adam Networks to contact
    me.  Make the extension work in all browsers that support
    webextensions.  Untested.

--- src/worker.js
+++ src/worker.js
@@ -1,66 +1,30 @@
-var errorHandler = function (e) {
-    if (e.error == "net::ERR_CONNECTION_REFUSED" || e.error == "net::ERR_CONNECTION_RESET"
-        || e.error == "net::ERR_CONNECTION_CLOSED") {
-        /* Find if blocked URL is the same as the tab URL */
-        chrome.tabs.get(e.tabId, function (tab) {
-            if (e.url == tab.url) {
-                /* Extract host name from URL */
-                var u_parts = e.url.split('/');
-                var blocked_url = u_parts[2];
-                for (var i = 3; i < u_parts.length; i++) {
-                    blocked_url += "/" + u_parts[i];
-                }
+// "If any https page fails to load, a request should be made to
+//  http://mytools.management/isblocked?path=[domain].  If the status
+//  of the request is 200, the current page should continue to be
+//  displayed and nothing should happen.
+//
+//  If the mytools page returns 403, then the user should be
+//  redirected to http://mytools.management/isblocked?path=[domain]."
+//
+//  -- an email to Jon from R.P.
 
-                /* Update tab to block page */
-                chrome.tabs.update(e.tabId, {
-                    url: 'http://mytools.management/isblocked?path=' + blocked_url
-                });
+const url_base = 'http://mytools.management/isblocked?path=';
 
-                chrome.history.deleteUrl({
-                    url: 'http://mytools.management/isblocked?path=' + blocked_url
-                });
-            }
-        });
-    }
-}
-
-// Message handler to be used to check if this extension is installed
-chrome.runtime.onMessageExternal.addListener(function (message, sender, sendResponse) {
-    sendResponse(true);
-});
-
-var filter = { url: [{ 'schemes': ['https'] }] };
-chrome.webNavigation.onErrorOccurred.addListener(errorHandler, filter);
-
-
-chrome.webRequest.onHeadersReceived.addListener(
-    function (details) {
-        if (details.statusCode == 200) {
-            var storage_key = 'load_' + details.url.split("?path=")[1];
-            var reload_count = 0;
-            chrome.storage.local.get(storage_key, function (result) {
-                if (result == undefined) {
-                    result = 0;
-                }
-                reload_count = result;
-
-                if (reload_count > 7) {
-                    console.log('Do not reload to infinity.');
-                } else {
-                    var redirect_url = "http://" + details.url.split("?path=")[1];
-                    chrome.tabs.update(details.tabId, {
-                        url: redirect_url
-                    });
-                }
-
-                chrome.storage.local.set({
-                    storage_key: (reload_count + 1)
-                });
-            });
-        }
-    },
-    {
-        urls: [
-            "http://mytools.management/isblocked*"
-        ]
+chrome.webNavigation.onErrorOccurred.addListener((details) => {
+  if (details.url.startsWith(url_base)) {
+    // Avoid infinite recursion.
+    return;
+  }
+  let domain = (new URL(details.url)).hostname;
+  if (!domain) {
+    // Ignore about:blank, etc.
+    return;
+  }
+  let url = url_base + domain;
+  fetch(url)
+    .then((response) => {
+      if (403 == response.status) {
+        chrome.tabs.update(details.tabId, {url: url});
+      }
     });
+});
